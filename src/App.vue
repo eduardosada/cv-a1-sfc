8f33764f93a0036533d4924f0cef0d577c6c7533
Eduardo Sada <eduardosada@gmail.com>
2019-10-20 18:21:41 +0200

    welcome

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="MyApp">{{greetings}}</div>
+</template>
+
+<script>
+export default {
+	name: "app",
+
+	data() {
+		return {
+			greetings: "¡Hola gente linda!"
+		};
+	}
+};
+</script>
+
+<style>
+#MyApp {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2rem;
+}
+</style>
